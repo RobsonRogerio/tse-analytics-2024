ac9a9a5acaf2c53dca0cb3a6ff990a07ae7fffe4
Robson Pinheiro <robson.rogerio@gmail.com>
2025-04-08 16:03:07 -0300

    clusters criados

--- src/02_analise.py
+++ src/02_analise.py
@@ -26,11 +26,13 @@ TX_COR_RACA_PRETA_PARDA = df['TOTAL_COR_RACA_PRETA_PARDA'].sum() / df['TOTAL_CAN
 
 
 # %%
-plt.figure(dpi=400)
+
+plt.figure(dpi=360, figsize=(6,5.5))
 
 sn.scatterplot(data=df, 
                x='TX_GEN_FEM_BR', 
-               y='TX_COR_RACA_PRETA_BR')
+               y='TX_COR_RACA_PRETA_BR',
+               )
 
 texts = []
 for i in df['SG_PARTIDO']:
@@ -46,11 +48,101 @@ plt.title('Cor vs Gênero - Eleições 2024')
 plt.xlabel('Taxa de Mulheres')
 plt.ylabel('Taxa de Pessoas Pretas')
 
-plt.hlines(y=TX_COR_RACA_PRETA, xmin=0.30, xmax=0.55, colors='black', linestyles='--', label=f'% Candidaturas Pessoas Pretas: {100*TX_COR_RACA_PRETA:.0f}%')
-plt.vlines(x=TX_GEN_FEMININO, ymin=0.05, ymax=0.35, colors='tomato', linestyles='--', label=f'% Candidaturas Mulheres: {100*TX_GEN_FEMININO:.0f}%')
+plt.hlines(y=TX_COR_RACA_PRETA, xmin=0.30, xmax=0.55, colors='black', linestyles='--', label=f'% Pessoas Pretas: {100*TX_COR_RACA_PRETA:.0f}%')
+plt.vlines(x=TX_GEN_FEMININO, ymin=0.05, ymax=0.35, colors='tomato', linestyles='--', label=f'% Mulheres: {100*TX_GEN_FEMININO:.0f}%')
 
 plt.legend()
 
 plt.savefig('../img/partidos_cor_raca_genero.png')
 
+
+# %%
+plt.figure(dpi=360, figsize=(6,5.5))
+
+sn.scatterplot(data=df, 
+               x='TX_GEN_FEM_BR', 
+               y='TX_COR_RACA_PRETA_BR',
+               size='TOTAL_CANDIDATURAS',
+               sizes=(5,200)
+               )
+
+texts = []
+for i in df['SG_PARTIDO']:
+    data = df[df['SG_PARTIDO'] == i]
+    x = data['TX_GEN_FEM_BR'].values[0]
+    y = data['TX_COR_RACA_PRETA_BR'].values[0]
+    texts.append(plt.text(x, y, i, fontsize=9))
+
+adjust_text(texts, only_move={'points': 'y', 'texts': 'y'}, arrowprops=dict(arrowstyle="-"))
+
+plt.grid(True)
+plt.title('Cor vs Gênero - Eleições 2024')
+plt.xlabel('Taxa de Mulheres')
+plt.ylabel('Taxa de Pessoas Pretas')
+
+plt.hlines(y=TX_COR_RACA_PRETA, xmin=0.30, xmax=0.55, colors='black', linestyles='--', label=f'% Pessoas Pretas: {100*TX_COR_RACA_PRETA:.0f}%')
+plt.vlines(x=TX_GEN_FEMININO, ymin=0.05, ymax=0.35, colors='tomato', linestyles='--', label=f'% Mulheres: {100*TX_GEN_FEMININO:.0f}%')
+
+plt.legend()
+
+handles, labels = plt.gca().get_legend_handles_labels()
+handles = handles[5:]
+labels = labels[5:]  
+
+plt.legend(handles=handles, labels=labels)
+
+plt.savefig('../img/partidos_cor_raca_genero_bolha_size.png')
+
+# %%
+
+from sklearn import cluster
+
+X = df[['TX_GEN_FEM_BR', 'TX_COR_RACA_PRETA_BR']]
+
+model = cluster.KMeans(n_clusters=5)
+model.fit(X)
+
+df['clusterBR'] = model.labels_
+df.groupby(['clusterBR'])['TX_GEN_FEM_BR'].count()
+
+# %%
+plt.figure(dpi=360, figsize=(6,5.5))
+
+sn.scatterplot(data=df, 
+               x='TX_GEN_FEM_BR', 
+               y='TX_COR_RACA_PRETA_BR',
+               size='TOTAL_CANDIDATURAS',
+               sizes=(5,300),
+               hue='clusterBR',
+               palette='viridis',
+               alpha=0.6,
+               )
+
+texts = []
+for i in df['SG_PARTIDO']:
+    data = df[df['SG_PARTIDO'] == i]
+    x = data['TX_GEN_FEM_BR'].values[0]
+    y = data['TX_COR_RACA_PRETA_BR'].values[0]
+    texts.append(plt.text(x, y, i, fontsize=9))
+
+adjust_text(texts, only_move={'points': 'y', 'texts': 'y'}, arrowprops=dict(arrowstyle="-"))
+
+plt.grid(True)
+plt.suptitle('Partidos: Cor vs Gênero - Eleições 2024')
+plt.title('Tamanho bolha = Qtde. Candidatos do Partido', fontdict={'size': 9})
+plt.xlabel('Taxa de Mulheres')
+plt.ylabel('Taxa de Pessoas Pretas')
+
+plt.hlines(y=TX_COR_RACA_PRETA, xmin=0.30, xmax=0.55, colors='black', alpha=0.6, linestyles='--', label=f'% Pessoas Pretas: {100*TX_COR_RACA_PRETA:.0f}%')
+plt.vlines(x=TX_GEN_FEMININO, ymin=0.05, ymax=0.35, colors='tomato', alpha=0.6, linestyles='--', label=f'% Mulheres: {100*TX_GEN_FEMININO:.0f}%')
+
+plt.legend()
+
+handles, labels = plt.gca().get_legend_handles_labels()
+handles = handles[12:]
+labels = labels[12:]  
+
+plt.legend(handles=handles, labels=labels)
+
+plt.savefig('../img/partidos_cor_raca_genero_clusterbr.png')
 # %%
